6e30c0a363ab58a92a90dc71874e75fee23fb4ec
thi174hcmus@gmail.com <thi174hcmus@gmail.com>
2021-05-28 14:50:22 +0700

    Revert "Add full initialize database"
    This reverts commit e6d8e60970a30ff40296e4fb9e84f941a418b4d4.

--- chatbot/src/handler.js
+++ chatbot/src/handler.js
@@ -1,69 +1,51 @@
-const fetch = require('node-fetch');
-const db = require('./db');
 
+const request = require('request');
+
+const fbGraphProfile = 'https://graph.facebook.com/v2.6/me/messenger_profile';
+const fbGraphMessages = 'https://graph.facebook.com/v2.6/me/messages';
 const accessToken = process.env.FBAPP_ACCESS_TOKEN;
-
-// generate Facebook api urls with query params
-const apiFbGraphProfile = 'https://graph.facebook.com/v2.6/me/messenger_profile';
-const urlFbGraphProfile = new URL(apiFbGraphProfile);
-urlFbGraphProfile.searchParams.append('access_token', accessToken);
-const apiFbGraphMessages = 'https://graph.facebook.com/v2.6/me/messages';
-const urlFbGraphMessages = new URL(apiFbGraphMessages);
-urlFbGraphMessages.searchParams.append('access_token', accessToken);
-
-const defaultHeaders = { 'Content-Type': 'application/json' };
-const payloadSearch = 'SEARCH_COURSES';
+const payloadMenuSearch = 'SEARCH_COURSES';
 const payloadCategories = 'CATEGORIES';
-const payloadCategoryPrefix = 'CATEGORY_';
-const defaultUrl = 'http://localhost';
-
-
-/* Initialize and enable persistent menu on messenger chat box */
-
-const postFetch = (url, headers, body) => {
-  fetch(url, {
-    method: "POST",
-    headers,
-    body: JSON.stringify(body)
-  });
-}
 
 
 /* Initialize and enable persistent menu on messenger chat box */
 
 const initPersistentMenu = _ => {
-  try {
-    const body = {
-      persistent_menu: [
-        {
-          locale: "default",
-          composer_input_disabled: false,
-          call_to_actions: [
-            {
-              type: "postback",
-              title: "Tìm kiếm khóa học",
-              payload: payloadSearch
-            },
-            {
-              type: "postback",
-              title: "Danh mục khóa học",
-              payload: payloadCategories
-            },
-          ]
-        }
-      ]
-    };
+  const body = {
+    persistent_menu: [
+      {
+        locale: "default",
+        composer_input_disabled: false,
+        call_to_actions: [
+          {
+            type: "postback",
+            title: "Tìm kiếm khóa học",
+            payload: payloadMenuSearch
+          },
+          {
+            type: "postback",
+            title: "Danh mục khóa học",
+            payload: payloadCategories
+          },
+        ]
+      }
+    ]
+  };
 
-    postFetch(urlFbGraphProfile, defaultHeaders, body);
-  } catch (error) {
-    console.error(error);
-  }
+  request({
+    url: fbGraphProfile,
+    qs: {
+      access_token: accessToken,
+    },
+    method: "POST",
+    json: body
+  });
 }
 
 
 /* Send single message to user */
 
-const sendMessage = async (senderId, message) => {
+const sendMessage = (senderId, message) => {
   const body = {
     recipient: {
       id: senderId
@@ -73,13 +55,20 @@ const sendMessage = async (senderId, message) => {
     }
   };
 
-  await postFetch(urlFbGraphMessages, defaultHeaders, body);
+  request({
+    url: fbGraphMessages,
+    qs: {
+      access_token: accessToken,
+    },
+    method: 'POST',
+    json: body
+  });
 }
 
 
 /* Send menu to user */
 
-const sendMenu = async senderId => {
+const sendMenu = senderId => {
   const body = {
     recipient: {
       id: senderId
@@ -94,7 +83,7 @@ const sendMenu = async senderId => {
             {
               type: "postback",
               title: "Tìm kiếm khóa học",
-              payload: payloadSearch
+              payload: payloadCategories
             },
             {
               type: "postback",
@@ -107,24 +96,20 @@ const sendMenu = async senderId => {
     }
   };
 
-  await postFetch(urlFbGraphMessages, defaultHeaders, body);
+  request({
+    url: fbGraphMessages,
+    qs: {
+      access_token: accessToken,
+    },
+    method: 'POST',
+    json: body
+  });
 }
 
 
 /* Send category list to user */
 
-const sendCategories = async senderId => {
-  const list = await db('category').select('id', 'name');
-  const listEls = list.map(item => ({
-    title: "Các khóa học",
-    buttons: [
-      {
-        title: item.name,
-        type: "postback",
-        payload: `${payloadCategoryPrefix}${item.id}`
-      }
-    ]
-  }));
+const sendCategories = senderId => {
   const body = {
     recipient: {
       id: senderId
@@ -135,11 +120,33 @@ const sendCategories = async senderId => {
         payload: {
           template_type: "list",
           top_element_style: "compact",
-          elements: listEls,
+          elements: [
+            {
+              title: "Các khóa học",
+              buttons: [
+                {
+                  title: "Làm giàu",
+                  type: "postback",
+                  payload: "payload"
+                }
+              ]
+            },
+            {
+              title: "Các khóa học",
+              buttons: [
+                {
+                  title: "Lập trình",
+                  type: "postback",
+                  payload: "payload"
+                }
+              ]
+            },
+          ],
           buttons: [
             {
               title: "View More",
-              type: "postback"
+              type: "postback",
+              payload: "payload"
             }
           ]
         }
@@ -147,32 +154,20 @@ const sendCategories = async senderId => {
     }
   };
 
-  await postFetch(urlFbGraphMessages, defaultHeaders, body);
-}
-
-
-/* Slice category id to id */
-
-const sliceId = payload => {
-  return +payload.split('_')[1]
+  request({
+    url: fbGraphMessages,
+    qs: {
+      access_token: accessToken,
+    },
+    method: 'POST',
+    json: body
+  });
 }
 
 
 /* Send course list to user */
 
-const sendCourses = async (senderId, datalist) => {
-  const listEls = datalist.map(item => ({
-    title: item.name,
-    subtitle: item.description,
-    image_url: item.image_messenger,
-    buttons: [
-      {
-        title: "Xem chi tiết",
-        type: "web_url",
-        url: item.url || defaultUrl,
-      },
-    ]
-  }));
+const sendCourses = (senderId, ) => {
   const body = {
     recipient: {
       id: senderId
@@ -183,11 +178,38 @@ const sendCourses = async (senderId, datalist) => {
         payload: {
           template_type: "list",
           top_element_style: "compact",
-          elements: listEls,
+          elements: [
+            {
+              title: "Học kinh doanh từ con số 0",
+              subtitle: "Thánh lùa gà",
+              image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-6y1-ILXV5PlvQKG5QVPuGFa1_Qxr6fuARA&usqp=CAU",
+              buttons: [
+                {
+                  title: "Tham gia ngay",
+                  type: "web_url",
+                  url: "http://localhost",
+                },
+              ]
+            },
+            {
+              title: "Học kinh doanh từ con số 0",
+              subtitle: "Thánh lùa gà",
+              image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-6y1-ILXV5PlvQKG5QVPuGFa1_Qxr6fuARA&usqp=CAU",
+
+              buttons: [
+                {
+                  title: "Tham gia ngay",
+                  type: "web_url",
+                  url: "http://localhost",
+                },
+              ]
+            },
+          ],
           buttons: [
             {
               title: "View More",
               type: "postback",
+              payload: "payload"
             }
           ]
         }
@@ -195,122 +217,52 @@ const sendCourses = async (senderId, datalist) => {
     }
   };
 
-  await postFetch(urlFbGraphMessages, defaultHeaders, body);
-}
-
-const sendCoursesByCategory = async (senderId, payload) => {
-  const categoryId = sliceId(payload);
-  const list = await db('course')
-    .where({ id_category: categoryId })
-    .select('id', 'name', 'description', 'image_messenger', 'url');
-
-  await sendCourses(senderId, list);
-}
-
-const sendCoursesByKeyword = async (senderId, keyword) => {
-  const list = await db('course')
-    .where('name', 'like', `%${keyword}%`)
-    .select('id', 'name', 'description', 'image_messenger', 'url');
-
-    if(list.length){
-      await sendCourses(senderId, list);
-    }
-    else {
-      sendMessage(senderId, 'Không tìm thấy!');
-      sendMenu(senderId);
-    }
+  request({
+    url: fbGraphMessages,
+    qs: {
+      access_token: accessToken,
+    },
+    method: 'POST',
+    json: body
+  });
 }
 
 
-/*  */
+const handleMessage = entries => {
+  for (let entry of entries) {
+    const messaging = entry.messaging;
+    for (let message of messaging) {
+      const senderId = message.sender.id;
+      console.log('message :>> ', message);
 
-const getSearchingState = async (fbUserId) => {
-  const doc = await db('state').where({
-    id_fb_user: fbUserId
-  })
-    .select('is_searching')
-
-  let isSearching = false;
-  if (doc[0]) {
-    const is_searching = doc[0].is_searching;
-    isSearching = is_searching;
-  }
-  return isSearching;
-}
-/*  */
-
-const setSearchingState = async (fbUserId, isSeartching) => {
-  await db('state').insert({
-    id_fb_user: fbUserId,
-    is_searching: !!isSeartching
-  })
-    .onConflict("id_fb_user")
-    .merge()
-    .returning("*");
-}
-
-
-/* Handle messaging */
-
-const handleMessage = async entries => {
-  try {
-    for (let entry of entries) {
-      const messaging = entry.messaging;
-      for (let message of messaging) {
-        const senderId = message.sender.id;
-
-        console.log('message :>> ', message);
-        // Check if user clicked a postback button or menu
-        console.log('message.postback :>> ', message.postback);
-        console.log('message.message :>> ', message.message);
-        if (message.postback) {
-          if (message.postback.payload) {
-            const payload = message.postback.payload;
-
-            if (payload !== payloadSearch) {
-              await setSearchingState(senderId, false)
-            }
-
-            if (payload === payloadSearch) {
-              sendMessage(senderId, 'Nhập tên khóa học: ');
-              await setSearchingState(senderId, true);
-            }
-            else if (payload === payloadCategories) {
-              sendMessage(senderId, 'Danh mục: ');
-              await sendCategories(senderId);
-            }
-            else if (new RegExp(`^${payloadCategoryPrefix}(\\d+)$`).test(payload)) {
-              sendCoursesByCategory(senderId, payload);
-            }
-            else {
-              sendMenu(senderId);
-            }
+      // Check if user clicked a postback button or menu
+      if (message.postback) {        
+        if (message.postback.payload) {
+          const payload = message.postback.payload;
+          console.log('message.postback.payload :>> ', payload);
+          if (payload === payloadCategories) {
+            sendMessage(senderId, 'Danh mục: ');
+            sendCategories(senderId);
+          }
+          else if (payload === payloadMenuSearch) {
+            sendMessage(senderId, 'Nhập tên khóa học: ');
+          }
+          else if (payload === 'payload') {
+            sendCourses(senderId, ': ');
+          }
+          else {
+            sendMenu(senderId);
           }
         }
-
-        else {
-          if (message.message) {
-            if (message.message.text) {
-              console.log('message.message.text :>> ', message.message.text);
-              const isSearching = await getSearchingState(senderId);
-              console.log('isSearching :>> ', isSearching);
-              const keyword = message.message.text;
-              if (isSearching) {
-                sendCoursesByKeyword(senderId, keyword);
-              }
-              else{
-                sendMenu(senderId);
-              }
-              await setSearchingState(senderId, false)
-            }
+      }
+      else{
+        if (message.message) {
+          if (message.message.text) {
+            sendMenu(senderId);
           }
         }
-
       }
     }
-
-  } catch (error) {
-    console.error(error);
   }
 }
 
