e6d8e60970a30ff40296e4fb9e84f941a418b4d4
thi174hcmus@gmail.com <thi174hcmus@gmail.com>
2021-05-28 14:39:22 +0700

    Add full initialize database

--- chatbot/src/handler.js
+++ chatbot/src/handler.js
@@ -1,51 +1,69 @@
+const fetch = require('node-fetch');
+const db = require('./db');
 
-const request = require('request');
-
-const fbGraphProfile = 'https://graph.facebook.com/v2.6/me/messenger_profile';
-const fbGraphMessages = 'https://graph.facebook.com/v2.6/me/messages';
 const accessToken = process.env.FBAPP_ACCESS_TOKEN;
-const payloadMenuSearch = 'SEARCH_COURSES';
+
+// generate Facebook api urls with query params
+const apiFbGraphProfile = 'https://graph.facebook.com/v2.6/me/messenger_profile';
+const urlFbGraphProfile = new URL(apiFbGraphProfile);
+urlFbGraphProfile.searchParams.append('access_token', accessToken);
+const apiFbGraphMessages = 'https://graph.facebook.com/v2.6/me/messages';
+const urlFbGraphMessages = new URL(apiFbGraphMessages);
+urlFbGraphMessages.searchParams.append('access_token', accessToken);
+
+const defaultHeaders = { 'Content-Type': 'application/json' };
+const payloadSearch = 'SEARCH_COURSES';
 const payloadCategories = 'CATEGORIES';
+const payloadCategoryPrefix = 'CATEGORY_';
+const defaultUrl = 'http://localhost';
+
+
+/* Initialize and enable persistent menu on messenger chat box */
+
+const postFetch = (url, headers, body) => {
+  fetch(url, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(body)
+  });
+}
 
 
 /* Initialize and enable persistent menu on messenger chat box */
 
 const initPersistentMenu = _ => {
-  const body = {
-    persistent_menu: [
-      {
-        locale: "default",
-        composer_input_disabled: false,
-        call_to_actions: [
-          {
-            type: "postback",
-            title: "Tìm kiếm khóa học",
-            payload: payloadMenuSearch
-          },
-          {
-            type: "postback",
-            title: "Danh mục khóa học",
-            payload: payloadCategories
-          },
-        ]
-      }
-    ]
-  };
+  try {
+    const body = {
+      persistent_menu: [
+        {
+          locale: "default",
+          composer_input_disabled: false,
+          call_to_actions: [
+            {
+              type: "postback",
+              title: "Tìm kiếm khóa học",
+              payload: payloadSearch
+            },
+            {
+              type: "postback",
+              title: "Danh mục khóa học",
+              payload: payloadCategories
+            },
+          ]
+        }
+      ]
+    };
 
-  request({
-    url: fbGraphProfile,
-    qs: {
-      access_token: accessToken,
-    },
-    method: "POST",
-    json: body
-  });
+    postFetch(urlFbGraphProfile, defaultHeaders, body);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 
 /* Send single message to user */
 
-const sendMessage = (senderId, message) => {
+const sendMessage = async (senderId, message) => {
   const body = {
     recipient: {
       id: senderId
@@ -55,20 +73,13 @@ const sendMessage = (senderId, message) => {
     }
   };
 
-  request({
-    url: fbGraphMessages,
-    qs: {
-      access_token: accessToken,
-    },
-    method: 'POST',
-    json: body
-  });
+  await postFetch(urlFbGraphMessages, defaultHeaders, body);
 }
 
 
 /* Send menu to user */
 
-const sendMenu = senderId => {
+const sendMenu = async senderId => {
   const body = {
     recipient: {
       id: senderId
@@ -83,7 +94,7 @@ const sendMenu = senderId => {
             {
               type: "postback",
               title: "Tìm kiếm khóa học",
-              payload: payloadCategories
+              payload: payloadSearch
             },
             {
               type: "postback",
@@ -96,20 +107,24 @@ const sendMenu = senderId => {
     }
   };
 
-  request({
-    url: fbGraphMessages,
-    qs: {
-      access_token: accessToken,
-    },
-    method: 'POST',
-    json: body
-  });
+  await postFetch(urlFbGraphMessages, defaultHeaders, body);
 }
 
 
 /* Send category list to user */
 
-const sendCategories = senderId => {
+const sendCategories = async senderId => {
+  const list = await db('category').select('id', 'name');
+  const listEls = list.map(item => ({
+    title: "Các khóa học",
+    buttons: [
+      {
+        title: item.name,
+        type: "postback",
+        payload: `${payloadCategoryPrefix}${item.id}`
+      }
+    ]
+  }));
   const body = {
     recipient: {
       id: senderId
@@ -120,33 +135,11 @@ const sendCategories = senderId => {
         payload: {
           template_type: "list",
           top_element_style: "compact",
-          elements: [
-            {
-              title: "Các khóa học",
-              buttons: [
-                {
-                  title: "Làm giàu",
-                  type: "postback",
-                  payload: "payload"
-                }
-              ]
-            },
-            {
-              title: "Các khóa học",
-              buttons: [
-                {
-                  title: "Lập trình",
-                  type: "postback",
-                  payload: "payload"
-                }
-              ]
-            },
-          ],
+          elements: listEls,
           buttons: [
             {
               title: "View More",
-              type: "postback",
-              payload: "payload"
+              type: "postback"
             }
           ]
         }
@@ -154,20 +147,32 @@ const sendCategories = senderId => {
     }
   };
 
-  request({
-    url: fbGraphMessages,
-    qs: {
-      access_token: accessToken,
-    },
-    method: 'POST',
-    json: body
-  });
+  await postFetch(urlFbGraphMessages, defaultHeaders, body);
+}
+
+
+/* Slice category id to id */
+
+const sliceId = payload => {
+  return +payload.split('_')[1]
 }
 
 
 /* Send course list to user */
 
-const sendCourses = (senderId, ) => {
+const sendCourses = async (senderId, datalist) => {
+  const listEls = datalist.map(item => ({
+    title: item.name,
+    subtitle: item.description,
+    image_url: item.image_messenger,
+    buttons: [
+      {
+        title: "Xem chi tiết",
+        type: "web_url",
+        url: item.url || defaultUrl,
+      },
+    ]
+  }));
   const body = {
     recipient: {
       id: senderId
@@ -178,38 +183,11 @@ const sendCourses = (senderId, ) => {
         payload: {
           template_type: "list",
           top_element_style: "compact",
-          elements: [
-            {
-              title: "Học kinh doanh từ con số 0",
-              subtitle: "Thánh lùa gà",
-              image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-6y1-ILXV5PlvQKG5QVPuGFa1_Qxr6fuARA&usqp=CAU",
-              buttons: [
-                {
-                  title: "Tham gia ngay",
-                  type: "web_url",
-                  url: "http://localhost",
-                },
-              ]
-            },
-            {
-              title: "Học kinh doanh từ con số 0",
-              subtitle: "Thánh lùa gà",
-              image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-6y1-ILXV5PlvQKG5QVPuGFa1_Qxr6fuARA&usqp=CAU",
-
-              buttons: [
-                {
-                  title: "Tham gia ngay",
-                  type: "web_url",
-                  url: "http://localhost",
-                },
-              ]
-            },
-          ],
+          elements: listEls,
           buttons: [
             {
               title: "View More",
               type: "postback",
-              payload: "payload"
             }
           ]
         }
@@ -217,52 +195,122 @@ const sendCourses = (senderId, ) => {
     }
   };
 
-  request({
-    url: fbGraphMessages,
-    qs: {
-      access_token: accessToken,
-    },
-    method: 'POST',
-    json: body
-  });
+  await postFetch(urlFbGraphMessages, defaultHeaders, body);
+}
+
+const sendCoursesByCategory = async (senderId, payload) => {
+  const categoryId = sliceId(payload);
+  const list = await db('course')
+    .where({ id_category: categoryId })
+    .select('id', 'name', 'description', 'image_messenger', 'url');
+
+  await sendCourses(senderId, list);
+}
+
+const sendCoursesByKeyword = async (senderId, keyword) => {
+  const list = await db('course')
+    .where('name', 'like', `%${keyword}%`)
+    .select('id', 'name', 'description', 'image_messenger', 'url');
+
+    if(list.length){
+      await sendCourses(senderId, list);
+    }
+    else {
+      sendMessage(senderId, 'Không tìm thấy!');
+      sendMenu(senderId);
+    }
 }
 
 
-const handleMessage = entries => {
-  for (let entry of entries) {
-    const messaging = entry.messaging;
-    for (let message of messaging) {
-      const senderId = message.sender.id;
-      console.log('message :>> ', message);
+/*  */
 
-      // Check if user clicked a postback button or menu
-      if (message.postback) {        
-        if (message.postback.payload) {
-          const payload = message.postback.payload;
-          console.log('message.postback.payload :>> ', payload);
-          if (payload === payloadCategories) {
-            sendMessage(senderId, 'Danh mục: ');
-            sendCategories(senderId);
-          }
-          else if (payload === payloadMenuSearch) {
-            sendMessage(senderId, 'Nhập tên khóa học: ');
-          }
-          else if (payload === 'payload') {
-            sendCourses(senderId, ': ');
-          }
-          else {
-            sendMenu(senderId);
+const getSearchingState = async (fbUserId) => {
+  const doc = await db('state').where({
+    id_fb_user: fbUserId
+  })
+    .select('is_searching')
+
+  let isSearching = false;
+  if (doc[0]) {
+    const is_searching = doc[0].is_searching;
+    isSearching = is_searching;
+  }
+  return isSearching;
+}
+/*  */
+
+const setSearchingState = async (fbUserId, isSeartching) => {
+  await db('state').insert({
+    id_fb_user: fbUserId,
+    is_searching: !!isSeartching
+  })
+    .onConflict("id_fb_user")
+    .merge()
+    .returning("*");
+}
+
+
+/* Handle messaging */
+
+const handleMessage = async entries => {
+  try {
+    for (let entry of entries) {
+      const messaging = entry.messaging;
+      for (let message of messaging) {
+        const senderId = message.sender.id;
+
+        console.log('message :>> ', message);
+        // Check if user clicked a postback button or menu
+        console.log('message.postback :>> ', message.postback);
+        console.log('message.message :>> ', message.message);
+        if (message.postback) {
+          if (message.postback.payload) {
+            const payload = message.postback.payload;
+
+            if (payload !== payloadSearch) {
+              await setSearchingState(senderId, false)
+            }
+
+            if (payload === payloadSearch) {
+              sendMessage(senderId, 'Nhập tên khóa học: ');
+              await setSearchingState(senderId, true);
+            }
+            else if (payload === payloadCategories) {
+              sendMessage(senderId, 'Danh mục: ');
+              await sendCategories(senderId);
+            }
+            else if (new RegExp(`^${payloadCategoryPrefix}(\\d+)$`).test(payload)) {
+              sendCoursesByCategory(senderId, payload);
+            }
+            else {
+              sendMenu(senderId);
+            }
           }
         }
-      }
-      else{
-        if (message.message) {
-          if (message.message.text) {
-            sendMenu(senderId);
+
+        else {
+          if (message.message) {
+            if (message.message.text) {
+              console.log('message.message.text :>> ', message.message.text);
+              const isSearching = await getSearchingState(senderId);
+              console.log('isSearching :>> ', isSearching);
+              const keyword = message.message.text;
+              if (isSearching) {
+                sendCoursesByKeyword(senderId, keyword);
+              }
+              else{
+                sendMenu(senderId);
+              }
+              await setSearchingState(senderId, false)
+            }
           }
         }
+
       }
     }
+
+  } catch (error) {
+    console.error(error);
   }
 }
 
--- posgres/create-tables.sql
+++ posgres/create-tables.sql
@@ -1,22 +1,189 @@
-DROP TABLE IF EXISTS State CASCADE;
-CREATE TABLE State (
+-- On update table trigger
+CREATE OR REPLACE FUNCTION update_col_trig() RETURNS TRIGGER
+   LANGUAGE plpgsql AS
+$$BEGIN
+   NEW.updated_at := CURRENT_TIMESTAMP;
+   RETURN NEW;
+END;$$;
+
+-- Create tables
+-- Facebook messenger state table
+DROP TABLE IF EXISTS FbState CASCADE;
+CREATE TABLE FbState (
   id_fb_user VARCHAR(50) PRIMARY KEY,
   is_searching BOOLEAN
 );
 
-DROP TABLE IF EXISTS Category CASCADE;
-CREATE TABLE Category (
+-- User type table
+DROP TABLE IF EXISTS AccountType CASCADE;
+CREATE TABLE AccountType (
   id SERIAL PRIMARY KEY,
+  type VARCHAR(100) UNIQUE NOT NULL,
   name VARCHAR(100) NOT NULL
 );
 
+-- User table
+DROP TABLE IF EXISTS Account CASCADE;
+CREATE TABLE Account (
+  id SERIAL PRIMARY KEY,
+  id_type INT NOT NULL,
+  fullname VARCHAR(200) NOT NULL,
+  email VARCHAR(200) UNIQUE NOT NULL,
+  password VARCHAR(500),
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT fk_account_type FOREIGN KEY (id_type) REFERENCES AccountType (id)
+);
+CREATE TRIGGER set_updated_at_account
+BEFORE INSERT OR UPDATE ON Account
+FOR EACH ROW
+EXECUTE FUNCTION update_col_trig();
+
+-- Course category table
+DROP TABLE IF EXISTS Category CASCADE;
+CREATE TABLE Category (
+  id SERIAL PRIMARY KEY,
+  id_parent INT,
+  name VARCHAR(100) NOT NULL,
+  CONSTRAINT fk_category_parent FOREIGN KEY (id_parent) REFERENCES Category (id)
+);
+
+-- Course table
 DROP TABLE IF EXISTS Course CASCADE;
 CREATE TABLE Course (
   id SERIAL PRIMARY KEY,
   id_category INT NOT NULL,
+  id_created_by INT NOT NULL,
   name VARCHAR(100) NOT NULL,
-  description VARCHAR(100),
-  image_messenger VARCHAR(500),
+  description VARCHAR(200),
+  content VARCHAR(1000),
+  thumbnail VARCHAR(200),
+  tutition INT,
+  is_completed BOOLEAN,
+  image_messenger VARCHAR(200),
   url VARCHAR(500),
-  CONSTRAINT fk_course_category FOREIGN KEY (id_category) REFERENCES category (id)
+  is_draft BOOLEAN,
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT fk_course_category FOREIGN KEY (id_category) REFERENCES Category (id),
+  CONSTRAINT fk_course_createdby FOREIGN KEY (id_created_by) REFERENCES Account (id)
+);
+CREATE TRIGGER set_updated_at_course
+BEFORE INSERT OR UPDATE ON Course
+FOR EACH ROW
+EXECUTE FUNCTION update_col_trig();
+
+-- Rating table
+DROP TABLE IF EXISTS Rating CASCADE;
+CREATE TABLE Rating (
+  id SERIAL PRIMARY KEY,
+  id_course INT NOT NULL,
+  id_student INT NOT NULL,
+  score FLOAT NOT NULL,
+  comment VARCHAR(100),
+  updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT fk_rating_course FOREIGN KEY (id_course) REFERENCES Course (id),
+  CONSTRAINT fk_rating_student FOREIGN KEY (id_student) REFERENCES Account (id),
+  UNIQUE (id_course, id_student)
+);
+CREATE TRIGGER set_updated_at_rating
+BEFORE INSERT OR UPDATE ON Rating
+FOR EACH ROW
+EXECUTE FUNCTION update_col_trig();
+
+-- Promotion table
+DROP TABLE IF EXISTS Promotion CASCADE;
+CREATE TABLE Promotion (
+  id SERIAL PRIMARY KEY,
+  id_course INT NOT NULL,
+  price FLOAT NOT NULL,
+  description VARCHAR(200),
+  time_begin TIMESTAMPTZ,
+  time_end TIMESTAMPTZ,
+  CONSTRAINT fk_promotion_course FOREIGN KEY (id_course) REFERENCES Course (id)
+);
+
+-- Lesson table
+DROP TABLE IF EXISTS Lesson CASCADE;
+CREATE TABLE Lesson (
+  id SERIAL PRIMARY KEY,
+  id_course INT NOT NULL,
+  sort_order INT UNIQUE NOT NULL,
+  title VARCHAR(100),
+  description VARCHAR(500),
+  is_preview BOOLEAN,
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT fk_lesson_course FOREIGN KEY (id_course) REFERENCES Course (id)
+);
+CREATE TRIGGER set_updated_at_lesson
+BEFORE INSERT OR UPDATE ON Lesson
+FOR EACH ROW
+EXECUTE FUNCTION update_col_trig();
+
+-- Attachment type table
+DROP TABLE IF EXISTS AttachmentType CASCADE;
+CREATE TABLE AttachmentType (
+  id SERIAL PRIMARY KEY,
+  type VARCHAR(100) UNIQUE NOT NULL,
+  name VARCHAR(100) NOT NULL
+);
+
+-- Attachment table
+DROP TABLE IF EXISTS Attachment CASCADE;
+CREATE TABLE Attachment (
+  id SERIAL PRIMARY KEY,
+  id_lesson INT,
+  id_course INT,
+  url VARCHAR(200),
+  description VARCHAR(500),
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT fk_attachment_lesson FOREIGN KEY (id_lesson) REFERENCES Lesson (id),
+  CONSTRAINT fk_attachment_course FOREIGN KEY (id_course) REFERENCES Course (id)
+);
+CREATE TRIGGER set_updated_at_attachment
+BEFORE INSERT OR UPDATE ON Attachment
+FOR EACH ROW
+EXECUTE FUNCTION update_col_trig();
+
+-- User course detail table
+DROP TABLE IF EXISTS UserCourseDetail CASCADE;
+CREATE TABLE UserCourseDetail (
+  id_user INT NOT NULL,
+  id_course INT NOT NULL,
+  is_watchlist BOOLEAN,
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id_user, id_course),
+  CONSTRAINT fk_ucdetail_user FOREIGN KEY (id_user) REFERENCES Account (id),
+  CONSTRAINT fk_ucdetail_course FOREIGN KEY (id_course) REFERENCES Course (id)
+);
+CREATE TRIGGER set_updated_at_ucdetail
+BEFORE INSERT OR UPDATE ON UserCourseDetail
+FOR EACH ROW
+EXECUTE FUNCTION update_col_trig();
+
+-- User lession detail table
+DROP TABLE IF EXISTS UserLessonDetail CASCADE;
+CREATE TABLE UserLessonDetail (
+  id_user INT NOT NULL,
+  id_lesson INT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id_user, id_lesson),
+  CONSTRAINT fk_uldetail_user FOREIGN KEY (id_user) REFERENCES Account (id),
+  CONSTRAINT fk_uldetail_lesson FOREIGN KEY (id_lesson) REFERENCES Lesson (id)
+);
+CREATE TRIGGER set_updated_at_uldetail
+BEFORE INSERT OR UPDATE ON UserLessonDetail
+FOR EACH ROW
+EXECUTE FUNCTION update_col_trig();
+
+-- Refresh token table
+DROP TABLE IF EXISTS RefreshToken CASCADE;
+CREATE TABLE RefreshToken (
+  id_user INT PRIMARY KEY,
+  refresh_token VARCHAR(200),
+  CONSTRAINT fk_rftoken_user FOREIGN KEY (id_user) REFERENCES Account (id)
 );
